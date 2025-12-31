04023ccbe1ec4eaeec4da98b17fc363eaa591ff3
Abdoulkarim <17a.karim@gmail.com>
2025-12-31 16:25:48 +0000

    feat: Install redux, react router, Shadcn, tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <div>Hello world</div>;
+import { Button } from "@/components/ui/button";
+import { InfoIcon } from "lucide-react";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
 }
+
+export default App;
