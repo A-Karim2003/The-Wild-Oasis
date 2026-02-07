28677a0af476f149854e6d1191b211845bb8405a
Abdoulkarim <17a.karim@gmail.com>
2026-02-07 21:24:32 +0000

    feat(table): add filtering and sorting

--- src/features/bookings/BookingDataTable.jsx
+++ src/features/bookings/BookingDataTable.jsx
@@ -1,12 +1,43 @@
 import TableRenderer from "@/components/TableRenderer";
-import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
-import React from "react";
+import {
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import React, { useMemo } from "react";
+import { useSearchParams } from "react-router";
 
+const sortConfig = {
+  "date-recent": [{ id: "dates", desc: true }],
+  "date-oldest": [{ id: "dates", desc: false }],
+  "price-high": [{ id: "amount", desc: true }],
+  "price-low": [{ id: "amount", desc: false }],
+};
 export default function BookingDataTable({ data, columns }) {
+  const [searchParams] = useSearchParams();
+
+  //* applys columns filtering
+  const filterBy = searchParams.get("status");
+  const columnFilters = useMemo(
+    () => (filterBy ? [{ id: "status", value: filterBy }] : []),
+    [filterBy],
+  );
+
+  //* applys column sorting
+  const sortBy = searchParams.get("sortBy") || "date-recent";
+  const sorting = useMemo(() => sortConfig[sortBy], [sortBy]);
+
   const table = useReactTable({
     data,
     columns,
+    state: {
+      columnFilters,
+      sorting,
+    },
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
