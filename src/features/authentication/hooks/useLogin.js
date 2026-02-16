f3f2d1ad253ce557002569457205f55b2db61d73
A-Karim2003 <17a.karim@gmail.com>
2026-02-16 17:14:10 +0000

    sync across PCs

--- src/features/authentication/hooks/useLogin.js
+++ src/features/authentication/hooks/useLogin.js
@@ -1,18 +1,15 @@
 import { login } from "@/services/apiAuth";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 
 export function useLogin() {
   const navigate = useNavigate();
-  const location = useLocation();
   const queryClient = useQueryClient();
-  console.log(location);
 
   return useMutation({
     mutationFn: login,
     onSuccess: (data) => {
-      console.log(data);
       queryClient.setQueriesData(["user"], data.user);
       navigate("/dashboard", { replace: true });
     },
--- src/features/authentication/hooks/useUser.js
+++ src/features/authentication/hooks/useUser.js
@@ -6,5 +6,6 @@ export default function useUser() {
     queryFn: getCurrentUser,
     queryKey: ["user"],
     retry: false, // Don't retry if user is not authenticated
+    staleTime: 5 * 60 * 1000, // data stays fresh for 5 min, reduces API calls
   });
 }
